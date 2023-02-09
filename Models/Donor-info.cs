9cdd0e0816c64a677855ff8a3c2aa6e6ac5aa51a
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2023-02-09 23:32:59 +0600

    donor info collect

---
+++ Models/Donor-info.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+
+
+namespace Blood_Donor_App_v4.Models
+{
+    public class Donor_info
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string  Phone_number { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
