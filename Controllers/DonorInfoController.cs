082c7cd8fa6ac0f25f38e1a194f60b33d2a0b548
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2023-02-10 22:12:14 +0600

    Donor

---
+++ Controllers/DonorInfoController.cs
@@ -0,0 +1,41 @@
+﻿using Blood_Donor_App_v4.Data;
+using Blood_Donor_App_v4.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blood_Donor_App_v4.Controllers
+{
+    public class DonorInfoController : Controller
+    {
+        
+        private readonly Blood_Donor_App_v4Context _context;
+
+        public DonorInfoController(Blood_Donor_App_v4Context context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        //Get:DonorInfo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,PhoneNumber,Gender,Address")] DonorInfo donorinfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(donorinfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(donorinfo);
+        }
+
+    }
+}
--- Models/DonorInfo.cs
+++ Models/DonorInfo.cs
@@ -1,11 +1,14 @@
 ﻿using Blood_Donor_App_v4.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blood_Donor_App_v4.Models
 {
-    public class DonorInfo
+    public class DonorInfo: IdentityUser
     {
         [Required]
+        [Key]
         public int Id { get; set; }
         [Required]
         public string  PhoneNumber { get; set; }
@@ -15,6 +18,11 @@ namespace Blood_Donor_App_v4.Models
         [Required]
         public string Address { get; set; }
 
+        
+/*        public string RegId { get; set; }
+        [ForeignKey("RegId")]
+        public RegisterModel RegisterModel { get; set; }
+*/
     }
         
 }
