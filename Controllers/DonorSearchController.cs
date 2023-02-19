ece9df6290214ae4ce2ebcd4939a79a03121de76
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2023-02-19 11:14:57 +0600

    email add start

--- Controllers/DonorSearchController.cs
+++ Controllers/DonorSearchController.cs
@@ -1,6 +1,8 @@
 ﻿using Blood_Donor_App_v4.Data;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
 
 namespace Blood_Donor_App_v4.Controllers
 {
@@ -35,5 +37,28 @@ namespace Blood_Donor_App_v4.Controllers
 
             /*return View(await _context.DonorOtherInfo.ToListAsync();*/
         }
+        public async Task<IActionResult> Mail()
+        {
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress("Donor", "190204084@aust.edu"));
+            mailMessage.To.Add(new MailboxAddress("Omar", "omarfaruktasnim555@gmail.com"));
+            mailMessage.Subject = "subject";
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = "Hello"
+            };
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect("smtp.gmail.com", 587, true);
+                smtpClient.Authenticate("190204084@aust.edu", "boxohxtgacjnnlip");
+                smtpClient.Send(mailMessage);
+                smtpClient.Disconnect(true);
+            }
+            return (IActionResult)Task.CompletedTask;
+        }
+
+
+
     }
 }
