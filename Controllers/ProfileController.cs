3198b589e842c52d5544c88ea24510f8ba133d0e
Naimul Islam <naimulislam19149@gmail.com>
2023-02-21 09:07:25 +0600

    Authorize done

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blood_Donor_App_v4.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blood_Donor_App_v4.Controllers
 {
+	[Authorize]
 	public class ProfileController : Controller
 	{
+
 		public IActionResult Index()
 		{
 			return View();
